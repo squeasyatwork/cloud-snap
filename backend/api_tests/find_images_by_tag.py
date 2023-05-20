3457d16c325b34c212798ede149fbb15bcd38745
Taha12k <tosm0001@student.monash.edu>
2023-05-20 15:24:31 +1000

    Changed image field to image_url

--- backend/api_tests/find_images_by_tag.py
+++ backend/api_tests/find_images_by_tag.py
@@ -11,7 +11,7 @@ def find_images_based_on_tags(dbRecords, tagBundles):
     for dbRecord in dbRecords:
         if all(tag in dbRecord["objects"].keys() for tag in tags.keys()):
             if all(dbRecord["objects"][tag] >= tags[tag] for tag in tags.keys()):
-                result.append(dbRecord["image"])
+                result.append(dbRecord["image_url"])
 
     return {"links": result}
 
@@ -20,12 +20,12 @@ if __name__ == "__main__":
     dbRecords = [
     {
         "id": "ae8311ba-25d0-5669-a892-45906810ee31",
-        "image": "000000141459.jpg",
+        "image_url": "000000141459.jpg",
         "objects": {"car": 2}
     },
     {
         "id": "2b807291-9781-5b61-9fde-08d856a1da41",
-        "image": "000000275236.jpg",
+        "image_url": "000000275236.jpg",
         "objects": {"person": 1, "skis": 1, "car": 1, "desk": 1}
     }
 ]

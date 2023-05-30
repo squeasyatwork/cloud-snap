ae900118fab6aebdd51b439d05b31a948367b236
Taha12k <tosm0001@student.monash.edu>
2023-05-30 11:54:59 +1000

    Updated task 2 lambda code to include search by image

--- backend/task2_lambda.py
+++ backend/task2_lambda.py
@@ -5,37 +5,32 @@ import numpy as np
 import sys
 import time
 import cv2
-import os
 
-# Lambda function
 def lambda_handler(event, context):
-    # Connecting to images table and retrieving records
+    # Connecting to images table
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('images')
     body = table.scan()
     dbRecords = body['Items']
     
-    # Find images based on the tags by handling API requests
+    # Find images based on the tags
     if str(event['resource']) == "/api/images/search/tags" and str(event['httpMethod']) == "POST":
         requestBody = json.loads(event['body'])
         tagBundles = requestBody['tags']
         
         result = []
         tags = {}
-        
-        # Creating a dictionary of tags and their counts
+    
         for tagBundle in tagBundles:
             tags[tagBundle['tag']] = tagBundle['count'] if "count" in tagBundle else 1
-        
-        # Filtering images based on the provided tags
+
         for dbRecord in dbRecords:
             if all(tag in dbRecord["objects"].keys() for tag in tags.keys()):
                 if all(dbRecord["objects"][tag] >= tags[tag] for tag in tags.keys()):
                     result.append(dbRecord["image_url"])
         
-        # Returning the search results as a JSON response
         return {
-            'statusCode': 201,
+            'statusCode': 200,
             'headers': {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
@@ -44,27 +39,119 @@ def lambda_handler(event, context):
         },
         'body': json.dumps({"links": result})
         }
-    # Find images based on the tags of an image by handling API requests
+    # Find images based on the tags of an image
     elif str(event['resource']) == "/api/images/search/image" and str(event['httpMethod']) == "POST":
         
         # Get image tags
-        requestBody = json.loads(event['body'])
-        id = requestBody["id"]
-        image = requestBody["image"]
+        image = json.loads(event["body"])["image"]
         
+       
+        # Image processing
+        # image_id = str(uuid.uuid5(uuid.NAMESPACE_OID, image))
+    
+        s3 = boto3.client('s3')
+        LABELS = s3.get_object(Bucket="fit5225-assignment2", Key="yolo_tiny_configs/coco.names")["Body"].read().decode('utf-8').strip().split("\n")
+        s3.download_file("fit5225-assignment2", "yolo_tiny_configs/yolov3-tiny.cfg", "/tmp/yolov3-tiny.cfg")
+        s3.download_file("fit5225-assignment2", "yolo_tiny_configs/yolov3-tiny.weights", "/tmp/yolov3-tiny.weights")
+    
+        decoded_data = base64.b64decode(image)
+        np_data = np.fromstring(decoded_data,np.uint8)
+        img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
         
-        # Copy code from previous if statement
+        npimg=np.array(img)
+        image=npimg.copy()
+        image=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    
+        net = cv2.dnn.readNetFromDarknet("/tmp/yolov3-tiny.cfg", "/tmp/yolov3-tiny.weights")
+        confthres = 0.3
+        nmsthres = 0.1
+
+        (H, W) = image.shape[:2]
+        # determine only the *output* layer names that we need from YOLO
+        ln = net.getLayerNames()
+        ln = [ln[i - 1] for i in net.getUnconnectedOutLayers().flatten()]
+
+        # construct a blob from the input image and then perform a forward
+        # pass of the YOLO object detector, giving us our bounding boxes and
+        # associated probabilities
+        blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+        net.setInput(blob)
+        layerOutputs = net.forward(ln)
+
+        # initialize our lists of detected bounding boxes, confidences, and
+        # class IDs, respectively
+        boxes = []
+        confidences = []
+        classIDs = []
+
+        # loop over each of the layer outputs
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability) of
+                # the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                # print(classID)
+                confidence = scores[classID]
+
+                # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > confthres:
+                    # scale the bounding box coordinates back relative to the
+                    # size of the image, keeping in mind that YOLO actually
+                    # returns the center (x, y)-coordinates of the bounding
+                    # box followed by the boxes' width and height
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY, width, height) = box.astype("int")
+
+                    # use the center (x, y)-coordinates to derive the top and
+                    # and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    # update our list of bounding box coordinates, confidences,
+                    # and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+
+                    confidences.append(float(confidence))
+                    classIDs.append(classID)
+
+                # apply non-maxima suppression to suppress weak, overlapping bounding boxes
+                idxs = cv2.dnn.NMSBoxes(boxes, confidences, confthres, nmsthres)
+
+                objects = []
+                if len(idxs) > 0:
+                    # loop over the indexes we are keeping
+                    for i in idxs.flatten():
+                        objects.append(
+                            {"label": LABELS[classIDs[i]], "accuracy": confidences[i], 
+                            "rectangle": {"height": boxes[i][3], "left": boxes[i][0], "top": boxes[i][1], "width": boxes[i][2]}}
+                            )
         
-        # Returning the image tags as a JSON response
+        tags = {}
+        for obj in objects:
+            if obj["accuracy"] > 0.6:
+                if obj["label"] not in tags:
+                    tags[obj["label"]] = 1
+                else:
+                    tags[obj["label"]] += 1
+        
+        result = []
+        for dbRecord in dbRecords:
+            if all(tag in dbRecord["objects"].keys() for tag in tags.keys()):
+                if all(dbRecord["objects"][tag] >= tags[tag] for tag in tags.keys()):
+                    result.append(dbRecord["image_url"])
+        
         return {
-            'statusCode': 201,
+            'statusCode': 200,
             'headers': {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
             'Access-Control-Allow-Credentials': 'true',
             'Content-Type': 'application/json'
         },
-        'body': str(image)
+        'body': json.dumps({"links": result})
         }
     # Manual addition or removal of tags
     elif str(event['resource']) == "/api/images/change/tags" and str(event['httpMethod']) == "POST":
@@ -108,7 +195,7 @@ def lambda_handler(event, context):
         )
         
         return {
-            'statusCode': 201,
+            'statusCode': 200,
             'headers': {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
@@ -131,8 +218,6 @@ def lambda_handler(event, context):
             s3 = boto3.client('s3')
             image_url_tokens = image_url.split("/")
             s3.delete_object(Bucket=image_url_tokens[-2], Key=image_url_tokens[-1]) 
-            
-            
             
             return {
                 'statusCode': 204,
